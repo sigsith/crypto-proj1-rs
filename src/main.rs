bb3cb33d121006d111b743f73c88045e5519e1f5
yifei-yao <yaoyifei0@gmail.com>
2024-03-19 11:29:08 -0400

    Set up main function

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,10 @@
-fn main() {}
+use std::io;
+fn main() -> Result<(), io::Error> {
+    println!("Enter the ciphertext:");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input)?;
+    let ciphertext = input.trim_end_matches('\n');
+    let result = String::new();
+    println!("My plaintext guess is:{result}");
+    Ok(())
+}
