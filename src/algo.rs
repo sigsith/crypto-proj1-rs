7ea4b7663a5fd23df2fc245b6777391859424eca
yifei-yao <yaoyifei0@gmail.com>
2024-03-19 12:03:10 -0400

    Add some validation

--- src/algo.rs
+++ src/algo.rs
@@ -2,5 +2,24 @@ pub fn apply_cryptanalysis(
     plaintext_candidates: &[&str],
     ciphertext: &str,
 ) -> Option<String> {
-    None
+    let (plaintext_len, ciphertext_len) =
+        verify_input(plaintext_candidates, ciphertext).ok()?;
+    Some("passed".to_string())
+}
+
+fn verify_input(
+    plaintext_candidates: &[&str],
+    ciphertext: &str,
+) -> Result<(usize, usize), ()> {
+    if plaintext_candidates.is_empty() {
+        return Err(());
+    }
+    let first_length = plaintext_candidates[0].len();
+    if !plaintext_candidates.iter().all(|s| s.len() == first_length) {
+        return Err(());
+    }
+    if ciphertext.len() < first_length {
+        return Err(());
+    }
+    Ok((first_length, ciphertext.len()))
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-#![warn(clippy::all, clippy::pedantic, clippy::nursery, clippy::cargo)]
+#![warn(clippy::all, clippy::pedantic)]
 #![warn(
     clippy::empty_structs_with_brackets,
     clippy::exit,
