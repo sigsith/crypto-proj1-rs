bd8b38398109aa2c9295f070c55bfb4d03547e41
yifei-yao <yaoyifei0@gmail.com>
2024-03-19 13:59:07 -0400

    More validation

--- src/algo.rs
+++ src/algo.rs
@@ -4,8 +4,8 @@ pub fn apply_cryptanalysis(
 ) -> Option<String> {
     let (plaintext_len, ciphertext_len) =
         verify_input(plaintext_candidates, ciphertext).ok()?;
-    // Some("passed".to_string())
-    None
+    Some("passed".to_string())
+    // None
 }
 
 fn verify_input(
@@ -15,12 +15,22 @@ fn verify_input(
     if plaintext_candidates.is_empty() {
         return Err(());
     }
+    if !is_lowercase_or_space(ciphertext) {
+        return Err(());
+    }
     let first_length = plaintext_candidates[0].len();
-    if !plaintext_candidates.iter().all(|s| s.len() == first_length) {
+    if !plaintext_candidates
+        .iter()
+        .all(|s| s.len() == first_length && is_lowercase_or_space(s))
+    {
         return Err(());
     }
     if ciphertext.len() < first_length {
         return Err(());
     }
     Ok((first_length, ciphertext.len()))
+}
+
+fn is_lowercase_or_space(s: &str) -> bool {
+    s.chars().all(|c| c.is_ascii_lowercase() || c == ' ')
 }
